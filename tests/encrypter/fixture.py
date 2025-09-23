8496e711b70c4a13d70638b52823b1a9c2e28e78
IvanDrf <ivandrgmr@gmail.com>
2025-09-24 01:02:30 +0300

    fix: add fake key for tests

--- tests/encrypter/fixture.py
+++ tests/encrypter/fixture.py
@@ -1,12 +1,16 @@
 import pytest
+from typing import Final
 
 from app.utils.encrypter import Encrypter
 from config.config import Config
 
+random_key: Final = 'u5HjK9n2XcQ7vEa1lP8gYmZpOqBwRkDyS3TfWxN0LtM='
+
 
 @pytest.fixture(scope='session')
 def encrypter() -> Encrypter:
-    cfg: Config = Config.New()
+    cfg: Config = Config(token='', db_name='',
+                         key=random_key)
 
     res: Encrypter = Encrypter.New(cfg)
     return res
